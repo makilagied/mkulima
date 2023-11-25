f3b57e5fdb41498505ea32eaa207882d5797bb62
makilagied <makialgied@gmail.com>
2023-11-25 15:13:52 +0300

    login page

--- src/app/user-pages/Login.js
+++ src/app/user-pages/Login.js
@@ -35,14 +35,6 @@ export class Login extends Component {
                     </div>
                     <a href="!#" onClick={event => event.preventDefault()} className="auth-link text-black">Forgot password?</a>
                   </div>
-                  <div className="mb-2">
-                    <button type="button" className="btn btn-block btn-facebook auth-form-btn">
-                      <i className="mdi mdi-facebook mr-2"></i>Connect using facebook
-                    </button>
-                  </div>
-                  <div className="text-center mt-4 font-weight-light">
-                    Don't have an account? <Link to="/user-pages/register" className="text-primary">Create</Link>
-                  </div>
                 </Form>
               </div>
             </div>
